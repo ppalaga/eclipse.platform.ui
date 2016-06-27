4b507590d765f8d9533e8582803ff4f242559e6f
Mickael Istria <mistria@redhat.com>
2016-06-27 18:28:27 +0200

    Bug 496747 - [Project Explorer] Fix path comparison
    The comparison was erroneous and led to unexpected ordering. This fixes
    the path comparison by using a segment-to-segment comparison rather than
    a plain toString one (which fails at properly understanding the meaning
    of a slash in path)
    
    Bug: 496747
    Change-Id: I7ee4d72b3656c407a4ab77e4a1b35d959d2667ef
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/PathComparator.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/PathComparator.java
@@ -23,7 +23,13 @@ public class PathComparator implements Comparator<IPath> {
 
 	@Override
 	public int compare(IPath arg0, IPath arg1) {
-		return arg0.toString().compareTo(arg1.toString());
+		for (int i = 0; i < Math.min(arg0.segmentCount(), arg1.segmentCount()); i++) {
+			int res = arg0.segment(i).compareTo(arg1.segment(i));
+			if (res != 0) {
+				return res;
+			}
+		}
+		return arg0.segmentCount() - arg1.segmentCount();
 	}
 
 }
